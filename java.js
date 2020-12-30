f8c6064937978d7362f1e31fca9f9d937b862bc6
Roko03 <rokoponjarac2@gmail.com>
2020-12-30 16:19:29 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,111 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper = new Swiper('.swiper-container0', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+  var swiper = new Swiper('.swiper-container1', {
+    slidesPerView: 2,
+    loop:true,
+    spaceBetween: 30,
+    freeMode:true,
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+  });
+
+  var swiper = new Swiper('.swiper-container12', {
+    slidesPerView: 4,
+    loop:true,
+    spaceBetween: 30,
+    freeMode:true,
+    slidesPerGroup: 2,
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper = new Swiper('.swiper-container13', {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 30,
+    freeMode:true,
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper = new Swiper('.swiper-container14', {
+    slidesPerView: 1,
+    loop:true,
+    spaceBetween: 30,
+    freeMode:true,
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var swiper=new Swiper('.swiper-container2',{
+      centeredSlides:true,
+      loop:true,
+      pagination:{
+          el:'.swiper-pagination',
+          clickable:true,
+      },
+      autoplay:{
+          delay:5500,
+          disableOnInteraction:false,
+      }
+  });
+
+  var swiper=new Swiper('.swiper-container22',{
+    centeredSlides:true,
+    loop:true,
+    autoplay:{
+        delay:5500,
+        disableOnInteraction:false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
